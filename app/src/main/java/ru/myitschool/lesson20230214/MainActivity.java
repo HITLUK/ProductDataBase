513226af87891cd080abf748123793952d79e5a4
ianakau <ianakau02@gmail.com>
2023-05-26 01:22:04 +0700

    LiveData, App, Repository

--- app/src/main/java/ru/myitschool/lesson20230214/MainActivity.java
+++ app/src/main/java/ru/myitschool/lesson20230214/MainActivity.java
@@ -1,95 +1,24 @@
 package ru.myitschool.lesson20230214;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-
-import android.app.SearchManager;
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
-import java.util.List;
-
+import androidx.appcompat.app.AppCompatActivity;
+import ru.myitschool.lesson20230214.data.DataBaseHelper;
 import ru.myitschool.lesson20230214.databinding.ActivityMainBinding;
+import ru.myitschool.lesson20230214.ui.main.MainFragment;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
-    private  SearchView searchView;
-
-    private ProductRepository repository = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-
-        if (repository == null) repository = ProductRepository.getInstance(getApplicationContext());
-
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.rootContainer);
-        if (fragment == null) {
-            fragment = new MainFragment();
-            fm.beginTransaction()
-                    // .addToBackStack(null)
-                    .replace(R.id.rootContainer, fragment, String.valueOf(false))
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.rootContainer, MainFragment.newInstance())
                     .commit();
         }
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        searchView = (SearchView) menu.findItem(R.id.action_search)
-                .getActionView();
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setMaxWidth(Integer.MAX_VALUE);
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-             //   adapter.getFilter().filter(query);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-               // adapter.getFilter().filter(newText);
-                return false;
-            }
-        });
-
-
-
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_search) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (!searchView.isIconified()) {
-            searchView.setIconified(true);
-            return;
-        }
-        super.onBackPressed();
-    }
-
 }
--- app/src/main/java/ru/myitschool/lesson20230214/data/ProductDao.java
+++ app/src/main/java/ru/myitschool/lesson20230214/data/ProductDao.java
@@ -1,23 +1,21 @@
 package ru.myitschool.lesson20230214.data;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 
-import java.util.Collection;
 import java.util.List;
-
-import ru.myitschool.lesson20230214.ProductData;
 
 @Dao
 public interface ProductDao {
-    @Query("SELECT * FROM productdata")
-    List<ProductData> getAll();
+    @Query("SELECT * FROM ProductData")
+    LiveData<List<ProductData>> getAll();
 
-    @Query("SELECT * FROM productdata WHERE title like:search or description like :search")
-    List<ProductData> getFromLike(String search);
+    @Query("SELECT * FROM ProductData WHERE title like :search or description like :search")
+    LiveData<List<ProductData>> getFromLike(String search);
 
     @Insert
     void insertAll(ProductData... productData);
